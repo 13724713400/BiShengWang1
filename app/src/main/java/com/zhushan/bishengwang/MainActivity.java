24fa2e310056d63006e9b2e62e8e095800613da1
毕昇网 <13724713400@163.com>
2016-03-08 11:17:26 +0800

    asdasdasdasdasdasd (cherry picked from commit af183d3a0a8ec857a24cd3f55840d3c7ed736053)

--- app/src/main/java/com/zhushan/bishengwang/MainActivity.java
+++ app/src/main/java/com/zhushan/bishengwang/MainActivity.java
@@ -84,6 +84,7 @@ public class MainActivity extends Basetivity{
         super.onCreate(savedInstanceState);
         setContentView(mainVu.initByLayout(this, R.layout.activity_main));
         initView();
+
         initData();
         initListener();
         main_homepage_tv.setTextColor(Color.parseColor("#ffdc2e"));
